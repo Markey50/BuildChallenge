d06865b47d0c83f824ea4e22745679a926282eaf
Markey50 <stonestage@gmail.com>
2013-09-08 00:40:29 -0500

    cleaned up more menus

--- src/com/github/Markey50/BuildChallenge/BuildChallengeCommandExecutor.java
+++ src/com/github/Markey50/BuildChallenge/BuildChallengeCommandExecutor.java
@@ -8,16 +8,14 @@ import org.bukkit.command.CommandSender;
 public class BuildChallengeCommandExecutor implements CommandExecutor {
 	
 	String header = ChatColor.WHITE + "BuildChallenge " + ChatColor.DARK_BLUE + "// ";
-	String menu1 = ChatColor.GREEN + "| " + ChatColor.AQUA;
-	String menu2 = ChatColor.WHITE + "- " + ChatColor.AQUA;
 	
 	String[] defaultMessage = new String[]{
 		ChatColor.WHITE + "Build Challege " + ChatColor.DARK_BLUE + "// " + ChatColor.AQUA + "Help Menu",
 		ChatColor.RED + "----------",
-		menu1 + "/build help admin " + menu2 + "Admin help commands",
-		menu1 + "/build help initiator " + menu2 + "Event initiator help commands",
-		menu1 + "/build help judge " + menu2 + "Judge help commands",
-		menu1 + "/build help builder " + menu2 + "Player help commands",
+		ChatColor.GREEN + "| " + ChatColor.AQUA + "/build help admin " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Admin help commands",
+		ChatColor.GREEN + "| " + ChatColor.AQUA + "/build help initiator " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Event initiator help commands",
+		ChatColor.GREEN + "| " + ChatColor.AQUA + "/build help judge " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Judge help commands",
+		ChatColor.GREEN + "| " + ChatColor.AQUA + "/build help builder " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Player help commands",
 		ChatColor.RED + "----------",
 		ChatColor.DARK_GRAY + "BuildChallenge plugin coded by " + ChatColor.DARK_RED + "M" + ChatColor.GOLD + "a" + ChatColor.YELLOW + "r" + ChatColor.GREEN + "k" + ChatColor.AQUA + "e" + ChatColor.DARK_BLUE + "y" + ChatColor.DARK_PURPLE + "5" + ChatColor.RED + "0"
 		};
@@ -65,9 +63,9 @@ public class BuildChallengeCommandExecutor implements CommandExecutor {
 						sender.sendMessage(new String[] {
 								header + ChatColor.RED + "ADMIN " + ChatColor.AQUA + "help commands",
 								ChatColor.RED + "----------",
-								menu1 + "/badmin arenacreate " + menu2 + "Walks through the steps of creating an arena",
-								menu1 + "/badmin setinitiator " + menu2 + "Add a player as a designated event initiator",
-								menu1 + "/badmin reminitiator " + menu2 + "Removes a player from the designated event initiator list",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/badmin arenacreate " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Walks through the steps of creating an arena",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/badmin setinitiator " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Add a player as a designated event initiator",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/badmin reminitiator " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Removes a player from the designated event initiator list",
 								ChatColor.RED + "----------"
 								});
 					break;
@@ -77,17 +75,17 @@ public class BuildChallengeCommandExecutor implements CommandExecutor {
 						sender.sendMessage(new String[] {
 								ChatColor.WHITE + "Build Challenge " + ChatColor.DARK_BLUE + "// " + ChatColor.BLUE + "INITIATOR " + ChatColor.AQUA + "help commands",
 								ChatColor.RED + "----------",
-								menu1 + "/bini initiate " + menu2 + "Start a build challenge event",
-								menu1 + "/bini invite <playername> " + menu2 + "Invite a player to the event",
-								menu1 + "/bini inviteall " + menu2 + "Invite all players to event",
-								menu1 + "/bini settime <time> <s, m, h> " + menu2 + "Set time limit for build challenge",
-								menu1 + "/bini start " + menu2 + "Officially start the challenge",
-								menu1 + "/bini timechange <new time> <s, m , h> " + menu2 + "Set the timer to the time specified",
-								menu1 + "/bini stop " + menu2 + "Force stop an event",
-								menu1 + "/bini viewjudges " + menu2 + "See list of current judges",
-								menu1 + "/bini invitejudge <player> " + menu2 + "Invite a player to judge",
-								menu1 + "/bini kickjudge <player> " + menu2 + "Kick a player from the judge list",
-								menu1 + "/bini winners <first> <second> <third>" + menu2 + "Select event winners (by cell number)",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini initiate " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Start a build challenge event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini invite <playername> " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Invite a player to the event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini inviteall " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Invite all players to event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini settime <time> <s, m, h> " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Set time limit for build challenge",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini start " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Officially start the challenge",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini timechange <new time> <s, m , h> " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Set the timer to the time specified",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini stop " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Force stop an event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini viewjudges " + ChatColor.WHITE + "- " + ChatColor.AQUA + "See list of current judges",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini invitejudge <player> " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Invite a player to judge",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini kickjudge <player> " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Kick a player from the judge list",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bini winners <first> <second> <third>" + ChatColor.WHITE + "- " + ChatColor.AQUA + "Select event winners (by cell number)",
 								ChatColor.RED + "----------"
 								});
 						break;
@@ -97,9 +95,9 @@ public class BuildChallengeCommandExecutor implements CommandExecutor {
 						sender.sendMessage(new String[] {
 								ChatColor.WHITE + "Build Challenge " + ChatColor.DARK_BLUE + "// " + ChatColor.GOLD + "INITIATOR " + ChatColor.AQUA + "help commands",
 								ChatColor.RED + "----------",
-								menu1 + "/bjudge register " + menu2 + "Registers you as a judge for the current event",
-								menu1 + "/bjudge resign " + menu2 + "Resign from being a judge for the current event",
-								menu1 + "/bjudge vote <cell number> " + ChatColor.WHITE + "-  " + ChatColor.AQUA + "Vote for a cell",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bjudge register " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Registers you as a judge for the current event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bjudge resign " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Resign from being a judge for the current event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/bjudge vote <cell number> " + ChatColor.WHITE + "-  " + ChatColor.AQUA + "Vote for a cell",
 								ChatColor.RED + "----------"
 								});
 					break;
@@ -109,11 +107,11 @@ public class BuildChallengeCommandExecutor implements CommandExecutor {
 						sender.sendMessage(new String[] {
 								ChatColor.WHITE + "Build Challenge " + ChatColor.DARK_BLUE + "// " + ChatColor.DARK_GREEN + "BUILDER " + ChatColor.AQUA + "help commands",
 								ChatColor.RED + "----------",
-								menu1 + "/builder accept " + menu2 + "Accept an invitation to a build challenge",
-								menu1 + "/builder decline " + menu2 + "Decline an invitation to a build challenge",
-								menu1 + "/builder info " + menu2 + "View information on the current event",
-								menu1 + "/builder leave " + menu2 + "Leave the current event",
-								menu1 + "/builder winnings " + menu2 + "Collect your winnings!",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/builder accept " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Accept an invitation to a build challenge",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/builder decline " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Decline an invitation to a build challenge",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/builder info " + ChatColor.WHITE + "- " + ChatColor.AQUA + "View information on the current event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/builder leave " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Leave the current event",
+								ChatColor.GREEN + "| " + ChatColor.AQUA + "/builder winnings " + ChatColor.WHITE + "- " + ChatColor.AQUA + "Collect your winnings!",
 								ChatColor.RED + "----------"
 								});
 					break;
